5746906cfa09220f6977fcfde900f6926fb9985f
Abdekrahman Khakifa <bodakhalifa707@gmail.com>
2025-03-27 21:21:31 +0200

    Laravel Project (Working with request Object & Query Prams)

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
 Route::get('/', function () {
     return view('welcome');
@@ -23,6 +24,22 @@ Route::any('/submit', function () {
     return "<a href='{$url}'>Click here</a>";
 });*/
 
-Route::get('/posts/{id}', function (string $id) {
+/*Route::get('/posts/{id}', function (string $id) {
     return ('post id: ' . $id);
-})->where('id', '[0-9]+');
+})->where('id', '[0-9]+');*/
+
+Route::get('/test', function (Request $request) {
+    return [
+        'method' => $request->method(),
+        'url' => $request->url(),
+        'path' => $request->path(),
+        'fullUrl' => $request->fullUrl(),
+        'ip' => $request->ip(),
+        'userAgent' => $request->userAgent(),
+        'header' => $request->header(),
+    ];
+});
+
+Route::get('/users', function (Request $request) {
+    return $request->all();
+});
