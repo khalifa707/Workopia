268048743aace4b6586ab342bec09b08457b0b4e
Abdekrahman Khakifa <bodakhalifa707@gmail.com>
2025-03-27 20:48:24 +0200

    Laravel Project (Workopia)

--- routes/web.php
+++ routes/web.php
@@ -8,17 +8,21 @@ Route::get('/', function () {
 
 Route::get('/jobs', function () {
     return ('available jobs');
-});
+})->name('jobs');
 
-Route::post('/submit', function () {
+/*Route::post('/submit', function () {
     return ('submitted');
 });
 
 Route::any('/submit', function () {
     return ('submitted');
-});
+});*/
 
 /*Route::get('/test', function () {
     $url = route('jobs');
     return "<a href='{$url}'>Click here</a>";
 });*/
+
+Route::get('/posts/{id}', function (string $id) {
+    return ('post id: ' . $id);
+})->where('id', '[0-9]+');
