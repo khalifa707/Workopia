a76653dff5c42a20c4f28d32549c680b534334fc
Abdekrahman Khakifa <bodakhalifa707@gmail.com>
2025-04-05 06:18:05 +0200

    Laravel Project (Working with Views & Controllers)

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -10,6 +10,7 @@
 <body>
 <h1>Create New Job</h1>
 <form action="/jobs" method="post">
+    @csrf
     <input type="text" name="title" placeholder="title">
     <input type="text" name="description" placeholder="description">
     <button type="submit">Submit</button>
