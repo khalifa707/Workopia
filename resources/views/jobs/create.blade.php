36cef1b5814397c185979abc652ef0ab7ec7ccd5
Abdekrahman Khakifa <bodakhalifa707@gmail.com>
2025-04-05 06:11:04 +0200

    Laravel Project (Working with Views & Controllers)

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -9,5 +9,9 @@
 </head>
 <body>
 <h1>Create New Job</h1>
+<form action="/jobs" method="post">
+    <input type="text" name="title" placeholder="title">
+    <input type="text" name="description" placeholder="description">
+</form>
 </body>
 </html>
