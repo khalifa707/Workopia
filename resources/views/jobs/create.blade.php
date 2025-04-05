11cd5529ce2114210aed54f5822aeb11474721fd
Abdekrahman Khakifa <bodakhalifa707@gmail.com>
2025-04-05 22:01:19 +0200

    Laravel Project (Working with Views & Controllers)

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -1,10 +1,10 @@
 <x-layout>
     <x-slot name="title">Create a job</x-slot>
-<h1>Create New Job</h1>
-<form action="/jobs" method="post">
-    @csrf
-    <input type="text" name="title" placeholder="title">
-    <input type="text" name="description" placeholder="description">
-    <button type="submit">Submit</button>
-</form>
+        <h1>Create New Job</h1>
+        <form action="/jobs" method="post">
+            @csrf
+            <input type="text" name="title" placeholder="title">
+            <input type="text" name="description" placeholder="description">
+            <button type="submit">Submit</button>
+        </form>
 </x-layout>
