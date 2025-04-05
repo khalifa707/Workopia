9f09b49bcdf30158c03b4f6ef9eb470f10fa5c50
Abdekrahman Khakifa <bodakhalifa707@gmail.com>
2025-04-05 08:33:09 +0200

    Laravel Project (Working with Views & Controllers)

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -7,30 +7,59 @@ use Illuminate\Http\Request;
 
 class JobController extends Controller
 {
-    public  function index()
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
     {
-        $title = 'Available Jobs';
-        $jobs = [
-            'Software Engineer',
-            'Web Developer',
-            'Data Scientist',
-        ];
-        return view('jobs.index', compact('title', 'jobs'));
+        //
     }
 
-    public function create(){
-        return view('jobs.create');
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
     }
 
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
     public function show(string $id)
     {
-       return "Showing job $id";
+        //
     }
 
-    public function store(request $request){
-        $title = $request->input('title');
-        $description = $request->input('description');
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
 
-        return "Title: $title Description: $description";
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
     }
 }
