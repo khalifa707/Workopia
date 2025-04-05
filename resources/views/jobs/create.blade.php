7cf79fe59ded40a3dc35386c528d07c0bdc7f201
Abdekrahman Khakifa <bodakhalifa707@gmail.com>
2025-04-05 20:55:07 +0200

    Laravel Project (Working with Views & Controllers)

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -1,13 +1,6 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Create New Job</title>
-</head>
-<body>
+@extends('layout')
+
+@section('content')
 <h1>Create New Job</h1>
 <form action="/jobs" method="post">
     @csrf
@@ -15,5 +8,4 @@
     <input type="text" name="description" placeholder="description">
     <button type="submit">Submit</button>
 </form>
-</body>
-</html>
+@endsection
