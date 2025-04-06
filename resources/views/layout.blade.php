dcbe610dac1bb4e91794ba8b074ff637109b24eb
Abdekrahman Khakifa <bodakhalifa707@gmail.com>
2025-04-06 22:03:14 +0200

    Laravel Project (Components and Styling)

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,12 +5,12 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
     @vite('resources/css/app.css')
     <title>{{$title ?? 'Workopia | Find and List Jobs'}}</title>
 </head>
 <body class="bg-gray-100">
 <x-header />
-<h1>Layout Component</h1>
 <main class="container mx-auto p-4 mt-4">
     {{$slot}}
 </main>
