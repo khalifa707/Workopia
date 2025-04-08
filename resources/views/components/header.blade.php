dc0d5918d0fe741923a92f8fed1f103323f2338f
Abdekrahman Khakifa <bodakhalifa707@gmail.com>
2025-04-08 19:20:51 +0200

    Laravel Project (Components and Styling)

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -23,17 +23,14 @@
         id="mobile-menu"
         class="hidden md:hidden bg-blue-900 text-white mt-5 pb-4 space-y-2"
     >
-        <a href="{{url('/jobs')}}" class="block px-4 py-2 hover:bg-blue-700">All Jobs</a>
-        <a href="{{url('/jobs/saved')}}" class="block px-4 py-2 hover:bg-blue-700"
-        >Saved Jobs</a
-        >
-        <a href="{{url('/dashboard')}}" class="block px-4 py-2 hover:bg-blue-700"
-        >Dashboard</a
-        >
-        <a href="{{url('/login')}}" class="block px-4 py-2 hover:bg-blue-700">Login</a>
-        <a href="{{url('/register')}}" class="block px-4 py-2 hover:bg-blue-700"
-        >Register</a
-        >
+        <x-nav-link url="/jobs" :active="request()->is('jobs')" :mobile="true">All Jobs</x-nav-link>
+        <x-nav-link url="/jobs/saved" :active="request()->is('jobs/saved')" :mobile="true">Saved Jobs</x-nav-link>
+
+        <x-nav-link url="/dashboard" :active="request()->is('dashboard')" :mobile="true">Dashboard</x-nav-link>
+
+        <x-nav-link url="/login" :active="request()->is('login')" :mobile="true">Login</x-nav-link>
+        <x-nav-link url="/Register" :active="request()->is('Register')" :mobile="true">Register</x-nav-link>
+
         <a
             href="{{url('/jobs/create')}}"
             class="block px-4 py-2 bg-yellow-500 hover:bg-yellow-600 text-black"
