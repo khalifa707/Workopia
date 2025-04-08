e6b0568ed28d4c5a664112783d57dd5a97e534bc
Abdekrahman Khakifa <bodakhalifa707@gmail.com>
2025-04-08 19:36:33 +0200

    Laravel Project (Components and Styling)

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -21,7 +21,7 @@
     <!-- Mobile Menu -->
     <div
         id="mobile-menu"
-        class=" md:hidden bg-blue-900 text-white mt-5 pb-4 space-y-2"
+        class=" hidden md:hidden bg-blue-900 text-white mt-5 pb-4 space-y-2"
     >
         <x-nav-link url="/jobs" :active="request()->is('jobs')" :mobile="true">All Jobs</x-nav-link>
         <x-nav-link url="/jobs/saved" :active="request()->is('jobs/saved')" :mobile="true">Saved Jobs</x-nav-link>
