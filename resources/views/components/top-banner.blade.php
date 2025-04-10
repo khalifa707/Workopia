d50e6b1ed15b79de09ac8d1e895b9aee97d42431
Abdekrahman Khakifa <bodakhalifa707@gmail.com>
2025-04-10 21:15:48 +0200

    Laravel Project (Components and Styling)

--- resources/views/components/top-banner.blade.php
+++ resources/views/components/top-banner.blade.php
@@ -1,3 +1,12 @@
-<div>
-    <!-- Well begun is half done. - Aristotle -->
-</div>
+@props(['heading'=>'Unlock Your Career Potential','subheading'=>'Discover the perfect job opportunity for you.'])
+
+<section class="bg-blue-900 text-white py-6 text-center">
+    <div class="container mx-auto">
+        <h2 class="text-3xl font-semibold">
+            {{$heading}}
+        </h2>
+        <p class="text-lg mt-2">
+            {{$subheading}}
+        </p>
+    </div>
+</section>
